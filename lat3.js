50f56378c78317623fe6480dde1f39ed4b7a611e
anozeref <kantorposfikry@gmail.com>
2025-09-18 15:07:04 +0700

    Form + Simpan Data di JSON

--- lat3.js
+++ lat3.js
@@ -1,14 +1,24 @@
+const fs = require('fs');
 const readln=require('readline');
 const rl=readln.createInterface({
     input:process.stdin,
     output:process.stdout,
 });
+const datapath='./data/contact.json';
+if (!fs.existsSync(datapath)){
+    fs.writeFileSync(datapath,'[]','utf-8');
+}
 
 rl.question ('Masukan Nama Anda: ', (nama)=>{
     rl.question ('Masukan Umur Anda: ', (umur)=>{
         rl.question ('Masukan Alamat Anda: ', (alamat)=>{
             rl.question ('Masukan Golongan Darah Anda: ', (goldar)=>{
-    console.log(`Terima Kasih, Saudara ${nama} yang berumur ${umur} tahun,`)
+        console.log(`Terima Kasih, Saudara ${nama} yang berumur ${umur} tahun,`)
         console.log(`dengan Golongan Darah ${goldar}, dan berasal dari ${alamat}`);
+        const kontak={nama:nama, umur:umur, alamat:alamat, goldar:goldar};
+        const file=fs.readFileSync('data/contact.json', 'utf-8');
+        const kontaks=JSON.parse(file);
+        kontaks.push(kontak);
+        fs.writeFileSync('data/contact.json',JSON.stringify(kontaks));
     rl.close()
 })})})});
